031effda87884bad3e99c7625944a76d79f48da3
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-10-17 15:53:26 -0500

    v1.0.7

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,41 +1,46 @@
 // 3rd Party
-import { reactive } from "vue";
-import { css, injectGlobal, keyframes } from "@emotion/css";
+import { reactive } from 'vue';
+import { css, injectGlobal, keyframes } from '@emotion/css';
 
 // Modules
-import VElement from "./VElement";
-import Sync from "./sync";
+import VElement from './VElement';
+import Sync from './sync';
 
 // Directives
-import swipe from "./directives/swipe";
-import resize from "./directives/resize";
-import clickOutside from "./directives/click-outside";
+import swipe from './directives/swipe';
+import resize from './directives/resize';
+import clickOutside from './directives/click-outside';
 
 export default {
-  install(app) {
-    // Global Device
-    app.config.globalProperties.$device = reactive({
-      width: 0,
-      height: 0,
-      type: null,
-      is: (...args) => args.includes(null),
-    });
-    // @Emotion-CSS
-    app.config.globalProperties.$style = {
-      css,
-      inject: injectGlobal,
-      keyframes: keyframes,
-    };
-    // @Super-Element
-    app.component("x-element", VElement);
-    // Sync
-    app.mixin({
-      mixins: [Sync],
-      directives: {
-        swipe,
-        resize,
-        clickOutside,
-      },
-    });
-  },
+	style: {
+		css,
+		inject: injectGlobal,
+		keyframes: keyframes,
+	},
+	install(app) {
+		// Global Device
+		app.config.globalProperties.$device = reactive({
+			width: 0,
+			height: 0,
+			type: null,
+			is: (...args) => args.includes(null),
+		});
+		// @Emotion-CSS
+		app.config.globalProperties.$style = {
+			css,
+			inject: injectGlobal,
+			keyframes: keyframes,
+		};
+		// @Super-Element
+		app.component('x-element', VElement);
+		// Sync
+		app.mixin({
+			mixins: [Sync],
+			directives: {
+				swipe,
+				resize,
+				clickOutside,
+			},
+		});
+	},
 };
